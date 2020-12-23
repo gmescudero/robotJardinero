321ffd6eaed09cf82971767e6b4fa07ee2c47547
Sergio Martinez <sergiomh499@gmail.com>
2020-12-23 13:39:15 +0100

    Update kalman.m

--- Codigo/Filtro Kalman/kalman.m
+++ Codigo/Filtro Kalman/kalman.m
@@ -1,28 +1,30 @@
 % Definimos una trayectoria circular
-velocidadL = 0.2;  % Velocidad lineal 0.2 m/seg
-timestep = 0.5;  % Actualizacion de sensores
-distanciatotal = 4*pi; % en metros.. radio de 2 metros
-numerodepasos = ceil(distanciatotal/(velocidadL*timestep));
-velocidadA = 2*pi/(numerodepasos*timestep); % Velocidad angular
+v = 0.1;  % Velocidad lineal 0.2 m/seg
+w = 0;
+h = 0.05;  % Actualizacion de sensores
 
-% Varianza del ruido del proceso 
-Qd = 0.01*velocidadL*timestep;
-Qb = 0.02*velocidadA*timestep;
-Qk_1 = [Qd 0; 0 Qb];
+% Posicion robot 
+robot.pos = [0, 0, 0];
+robot.ang = [pi/2];
+% Posicion del laser respecto al robot
+laser.pos(1) = 0.1;
+laser.pos(2) = 0;
+laser.ang = 0;
 
-for k = 1:2*numerodepasos
-    trayectoriaD(k) = velocidadL*timestep;
-    trayectoriaB(k) = velocidadA*timestep;
-    trayectoriaDRuido(k) = velocidadL*timestep + sqrt(Qd)*randn;
-    trayectoriaBRuido(k) = velocidadA*timestep + sqrt(Qb)*randn;
-end
+% Pasamos la posición del laser a cordenadas refenciales
+laserPosXRef = laser.pos(1)*cos(robot.ang) - laser.pos(2)*sin(robot.ang);
+laserPosYRef = laser.pos(1)*sin(robot.ang) + laser.pos(2)*cos(robot.ang);
+laserAngRef = robot.ang + laser.ang;
 
 % Inicializamos la posición inicial y su covarianza
-xini = 5;
-yini = 2;
-thetaini = 0;
-Xrealk = [xini; yini; thetaini];
+Xrealk = [robot.pos(1); robot.pos(2); robot.ang];
 Xk = [6; 3; pi];
+apoloPlaceMRobot('Marvin', robot.pos, robot.ang);
+
+% Varianza del ruido del proceso 
+Qd = 0.01*v*h;
+Qb = 0.02*w*h;
+Qk_1 = [Qd 0; 0 Qb];
 
 Pxini = 0.001;
 Pyini = 0.001;
@@ -35,57 +37,74 @@ R2 = 0.001;
 R3 = 0.001;
 Rk = [R1 0 0; 0 R2 0; 0 0 R3];
 
-% Posición de las balizas
-t1x = 4;
-t1y = 8;
-t2x = 1;
-t2y = 1;
-t3x = 11;
-t3y = 3;
+% Posicion balizas
+LM(1,:) = [-3.9, 0.0, 0.2];
+LM(2,:) = [-3.9, 3.9, 0.2];
+LM(3,:) = [0.0, 3.9, 0.2];
+LM(4,:) = [3.9, 3.9, 0.2];
+LM(5,:) = [3.9, 0.0, 0.2];
 
 % Algoritmo
+t = 0;
+tmax = 1;
+k = 0;
 Ktotal = zeros(3);      
-for l = 1:length(trayectoriaD)
-    % Solo para el simulador
-    XrealkAUX = Xrealk;
-    
+while t<tmax
+    k = k + 1;
     % Avance real del robot
-    Xrealk(1) = XrealkAUX(1) + trayectoriaD(l)*cos(XrealkAUX(3)+(trayectoriaB(l)/2));
-    Xrealk(2) = XrealkAUX(2) + trayectoriaD(l)*sin(XrealkAUX(3)+(trayectoriaB(l)/2));
-    Xrealk(3) = XrealkAUX(3) + trayectoriaB(l);
-    Xreal(:,l) = Xrealk;  % Para mantener una historia del recorrido
+    apoloMoveMRobot('Marvin', [v w], h);
+    XrealAUX = apoloGetLocationMRobot('Marvin');
+    
+    Xrealk(1) = XrealAUX(1);
+    Xrealk(2) = XrealAUX(2);
+    Xrealk(3) = XrealAUX(4);
+    Xreal(:,k) = Xrealk;  % Para mantener una historia del recorrido
 
-    % Observacion de las balizas
-    Zk = [(atan2(t1y-Xrealk(2),t1x-Xrealk(1)) - Xrealk(3) + sqrt(R1)*randn);
-          (atan2(t2y-Xrealk(2),t2x-Xrealk(1)) - Xrealk(3) + sqrt(R2)*randn);
-          (atan2(t3y-Xrealk(2),t3x-Xrealk(1)) - Xrealk(3) + sqrt(R3)*randn)];
-          
-    % Para acortar el nombre de la variable
-    Uk = [trayectoriaDRuido(l); trayectoriaBRuido(l)];
+      
+    % Se realiza una busqueda de balizas por el laser
+    baliza = apoloGetLaserLandMarks('LMS100');
+    x_sum = 0;
+    y_sum = 0;
+    ang_sum = 0;
+    for j = 1:size(baliza.id)
+        % Extaccion de las medidas realizadas por el laser
+        distancia_laser = baliza.distance(j);
+        angulo_laser = baliza.angle(j);
+        % Estimacion de la posicion a partir de los datos obtenidos por el
+        % laser
+        x_est_laser = - distancia_laser*sin(angulo_laser) + laserPosXRef;
+        y_est_laser = distancia_laser*cos(angulo_laser) + laserPosYRef;
+        ang_est_laser = angulo_laser + laserAngRef;
+        % Media estimacion
+        x_sum = x_sum + x_est_laser;
+        y_sum = y_sum + y_est_laser;
+        ang_sum = ang_sum + ang_est_laser;
+    end
+    
+    Zk = [x_sum/j; y_sum/j; ang_sum/j]; 
 
     % Nuevo ciclo, k-1 = k.
     Xk_1 = Xk;
     Pk_1 = Pk;
     
     % Prediccion del estado (Modelo de odometria)
-    X_k = [(Xk_1(1) + Uk(1)*cos(Xk_1(3)+(Uk(2)/2)));
-           (Xk_1(2) + Uk(1)*sin(Xk_1(3)+(Uk(2)/2)));
-           (Xk_1(3) + Uk(2))];
-    Ak = [1 0 (-Uk(1)*sin(Xk_1(3)+Uk(2)/2));
-          0 1 (Uk(1)*cos(Xk_1(3)+Uk(2)/2));
+    X_k = [(Xk_1(1) + v*cos(Xk_1(3)+w/2));
+           (Xk_1(2) + v*sin(Xk_1(3)+(w/2)));
+           (Xk_1(3) + w)];
+       
+    Ak = [1 0 (-v*sin(Xk_1(3)+w/2));
+          0 1 (v*cos(Xk_1(3)+w/2));
           0 0 1                             ]; % Fi
-    Bk = [(cos(Xk_1(3)+Uk(2)/2)) (-0.5*Uk(1)*sin(Xk_1(3)+Uk(2)/2));
-          (sin(Xk_1(3)+Uk(2)/2)) (0.5*Uk(1)*cos(Xk_1(3)+Uk(2)/2));
+      
+    Bk = [(cos(Xk_1(3)+w/2)) (-0.5*v*sin(Xk_1(3)+w/2));
+          (sin(Xk_1(3)+w/2)) (0.5*v*cos(Xk_1(3)+w/2));
            0                     1                                 ]; % G
+       
     P_k = Ak*Pk_1*((Ak)') + Bk*Qk_1*((Bk)');
 
     % Prediccion de la medida (Modelo de observacion)
-    Zk_ = [(atan2(t1y-X_k(2),t1x-X_k(1)) - X_k(3));
-          (atan2(t2y-X_k(2),t2x-X_k(1)) - X_k(3));
-          (atan2(t3y-X_k(2),t3x-X_k(1)) - X_k(3))];
-    Hk = [((t1y-X_k(2))/((t1x-X_k(1))^2+(t1y-X_k(2))^2)) (-(t1x-X_k(1))/((t1x-X_k(1))^2+(t1y-X_k(2))^2)) (-1);
-          ((t2y-X_k(2))/((t2x-X_k(1))^2+(t2y-X_k(2))^2)) (-(t2x-X_k(1))/((t2x-X_k(1))^2+(t2y-X_k(2))^2)) (-1);   
-          ((t3y-X_k(2))/((t3x-X_k(1))^2+(t3y-X_k(2))^2)) (-(t3x-X_k(1))/((t3x-X_k(1))^2+(t3y-X_k(2))^2)) (-1)];
+    Zk_ = Zk;
+    Hk = [1 0 0; 0 1 0; 0 0 1];
 
     % Comparacion
     Yk = Zk-Zk_;
@@ -105,73 +124,24 @@ for l = 1:length(trayectoriaD)
     Pk = (eye(3)-Wk*Hk)*P_k;
     
     %Sólo para almacenarlo
-    Xestimado(:,l) = Xk;
-    Pacumulado(1,l) = Pk(1,1);
-    Pacumulado(2,l) = Pk(2,2);
-    Pacumulado(3,l) = Pk(3,3);
+    Xestimado(:,k) = Xk;
+    Pacumulado(1,k) = Pk(1,1);
+    Pacumulado(2,k) = Pk(2,2);
+    Pacumulado(3,k) = Pk(3,3);
+    
+    % Actualizacion del tiempo
+    t = t+h;
+    apoloUpdate;
 end 
 
 % Representacion grafica
 figure(1);
-subplot(2,2,1);
 axis([0 12 0 9])
-hold on
 
-for m = 1:length(trayectoriaD)
-    plot(Xreal(1,m),Xreal(2,m),'.k', Xestimado(1,m),Xestimado(2,m),'.r');
-end
+plot(Xreal(:,1),Xreal(:,2),'r--')
+hold on
+plot(Xestimado(:,1),Xestimado(:,2),'b--')
+hold off
 legend('Movimiento Real','Estimación')
 
-% Balizas
-plot(t1x,t1y,'sk');
-plot(t2x,t2y,'sk');
-plot(t3x,t3y,'sk');
-xlabel ('X (m)')
-ylabel ('Y (m)')
-
-subplot(2,2,2);
-plot(Xreal(1,:));
-hold on
-plot(Xestimado(1,:),'g');
-xlabel ('t (muestras)')
-ylabel ('X (m)')
-
-subplot(2,2,3);
-plot(Xreal(2,:));
-hold on
-plot(Xestimado(2,:),'g');
-xlabel ('t (muestras)')
-ylabel ('Y (m)')
-
-subplot(2,2,4);
-plot(Xreal(3,:));
-hold on
-plot(Xestimado(3,:),'g');
-xlabel ('t (muestras)')
-ylabel ('\theta (rad)')
-hold off
-
-figure(2);
-subplot(3,1,1);
-axis([0 12 0 9])
-plot(Pacumulado(1,:),'b');
-xlabel ('t (muestras)')
-ylabel ('Varianza X (m2)')
-hold on
-
-subplot(3,1,2);
-axis([0 12 0 9])
-plot(Pacumulado(2,:),'b');
-xlabel ('t (muestras)')
-ylabel ('Varianza Y (m2)')
-
-subplot(3,1,3);
-axis([0 12 0 9])
-plot(Pacumulado(3,:),'b');
-xlabel ('t (muestras)')
-ylabel ('Varianza \theta (rad2)')
-
-pause
-close all
-clear all
 
